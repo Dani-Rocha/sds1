cade9b9c03fb7fad82ddbf197c5f35d4868544a9
Dani-Rocha <danynr2009@hotmail.com>
2020-09-15 14:47:51 -0300

    DTO, Service

--- backend/src/main/java/com/example/devsuperior/dspesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/example/devsuperior/dspesquisa/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.example.devsuperior.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.devsuperior.dspesquisa.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 	
 	
